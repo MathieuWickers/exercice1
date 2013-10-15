e19d97fc46b1d952e15a83b36c38c74ad2ba681d
MathieuWickers <wickers.mathieu@gmail.com>
2013-10-15 16:40:45 +0200

    with commentary

--- adder.js
+++ adder.js
@@ -1,21 +1,24 @@
-function adder(a, b){
-	return function(v){
-		return a(v) + b(v);
-	}
-}
-
+/** Multiplie le paramètre a avec b */
 function mult(a){
 	return function(b){
 		return a*b;
 	}
 }
 
+/** Soustrait le paramètre a à b */
 function sub(a) {
 	return function(b) {
 		return a-b;
 	}
 }
 
+sub(0)(0); // 0
+sub(2)(1); // 1
+sub(2)(2); // 0
+sub(2)(4); // -2
+
+
+/** Additionne n paramètres */
 function adder(){
 	var args = Array.prototype.slice.call(arguments);
 	return function(v){
@@ -26,3 +29,10 @@ function adder(){
 		return result;
 	}
 }
+
+adder()(0); // 0
+adder()(1); // 0
+adder(mult(2))(1); // 2
+adder(mult(2), mult(2))(1); // 4
+adder(mult(2), mult(2), mult(2))(1); // 6
+adder(mult(2), sub(2), mult(2))(1); // 5
